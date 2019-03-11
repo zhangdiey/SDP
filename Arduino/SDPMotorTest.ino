5027be57bc496e6113253edf90bff19e557ade90
kouwaivan <37375758+kouwaivan@users.noreply.github.com>
2019-03-11 09:53:14 +0000

    Add files via upload

--- Arduino/SDPMotorTest.ino
+++ Arduino/SDPMotorTest.ino
@@ -1,142 +1,104 @@
 #include "SDPArduino.h"
+#include "GroveColorSensor.h"
 #include <Wire.h>
+#define value_for_black 400
+#define value_for_white 100
 int i = 0;
+int red, blue, green;
+
+unsigned long timeDetected;
+unsigned long timeBlue;
+#define value_for_blue 40
+#define time_check 50
+#define time_stop 5
+#define turn_left 0
+#define turn_right 1
+#define go_forward 2
+bool detected = false;
+int state_turn = -1;
+
+bool blue_spot = false;
 
 void setup(){
   SDPsetup();
   helloWorld();
+
 }
 
 void loop(){
-Serial.println("All Motors Forward 50%");
-motorForward(0, 50);
-motorForward(1, 50);
-motorForward(2, 50);
-motorForward(3, 50);
-motorForward(4, 50);
-motorForward(5, 50);
-delay(2500);
-Serial.println("All Motors Forwards 100%");
-motorForward(0, 100);
-motorForward(1, 100);
-motorForward(2, 100);
-motorForward(3, 100);
-motorForward(4, 100);
-motorForward(5, 100);
-delay(2500);
-Serial.println("All Motors Backwards 50%");
-motorBackward(0, 50);
-motorBackward(1, 50);
-motorBackward(2, 50);
-motorBackward(3, 50);
-motorBackward(4, 50);
-motorBackward(5, 50);
-delay(2500);
-Serial.println("All Motors Backwards 100%");
-motorBackward(0, 100);
-motorBackward(1, 100);
-motorBackward(2, 100);
-motorBackward(3, 100);
-motorBackward(4, 100);
-motorBackward(5, 100);
-delay(2500);
-Serial.println("All Motors Stop");
-motorAllStop();
-delay(2500);
+    GroveColorSensor colorSensor;
+  colorSensor.ledStatus = 1;
+  colorSensor.readRGB(&red,&green,&blue);
+  Serial.println(blue);
+  if(blue > value_for_blue )
+  {
+    blue_spot = true;
+     
+  }
+  if(!blue_spot)
+  {
+    followLine();
+  }
+  else
+  {
+     motorAllStop();
+     delay(2500);
+     blue_spot = false;    
+  }
+}
 
-Serial.println("Motor 0 Forward 50%");
-motorForward(0, 50);
-delay(2500);
-Serial.println("Motor 0 Forwards 100%");
-motorForward(0, 100);
-delay(2500);
-Serial.println("Motor 0 Backwards 50%");
-motorBackward(0, 50);
-delay(2500);
-Serial.println("Motor 0 Backwards 100%");
-motorBackward(0, 100);
-delay(2500);
-Serial.println("Motor 0 Stop");
-motorStop(0);
-delay(2500);
-
-Serial.println("Motor 1 Forward 50%");
-motorForward(1, 50);
-delay(2500);
-Serial.println("Motor 1 Forwards 100%");
-motorForward(1, 100);
-delay(2500);
-Serial.println("Motor 1 Backwards 50%");
-motorBackward(1, 50);
-delay(2500);
-Serial.println("Motor 1 Backwards 100%");
-motorBackward(1, 100);
-delay(2500);
-Serial.println("Motor 1 Stop");
-motorStop(1);
-delay(2500);
-
-Serial.println("Motor 2 Forward 50%");
-motorForward(2, 50);
-delay(2500);
-Serial.println("Motor 2 Forwards 100%");
-motorForward(2, 100);
-delay(2500);
-Serial.println("Motor 2 Backwards 50%");
-motorBackward(2, 50);
-delay(2500);
-Serial.println("Motor 2 Backwards 100%");
-motorBackward(2, 100);
-delay(2500);
-Serial.println("Motor 2 Stop");
-motorStop(2);
-delay(2500);
-
-Serial.println("Motor 3 Forward 50%");
-motorForward(3, 50);
-delay(2500);
-Serial.println("Motor 3 Forwards 100%");
-motorForward(3, 100);
-delay(2500);
-Serial.println("Motor 3 Backwards 50%");
-motorBackward(3, 50);
-delay(2500);
-Serial.println("Motor 3 Backwards 100%");
-motorBackward(3, 100);
-delay(2500);
-Serial.println("Motor 3 Stop");
-motorStop(3);
-delay(2500);
-
-Serial.println("Motor 4 Forward 50%");
-motorForward(4, 50);
-delay(2500);
-Serial.println("Motor 4 Forwards 100%");
-motorForward(4, 100);
-delay(2500);
-Serial.println("Motor 4 Backwards 50%");
-motorBackward(4, 50);
-delay(2500);
-Serial.println("Motor 4 Backwards 100%");
-motorBackward(4, 100);
-delay(2500);
-Serial.println("Motor 4 Stop");
-motorStop(4);
-delay(2500);
-
-Serial.println("Motor 5 Forward 50%");
-motorForward(5, 50);
-delay(2500);
-Serial.println("Motor 5 Forwards 100%");
-motorForward(5, 100);
-delay(2500);
-Serial.println("Motor 5 Backwards 50%");
-motorBackward(5, 50);
-delay(2500);
-Serial.println("Motor 5 Backwards 100%");
-motorBackward(5, 100);
-delay(2500);
-Serial.println("Motor 5 Stop");
-motorStop(5);
-delay(2500);
+void detectBlack(){
+  if(readAnalogSensorData(3)>value_for_black)
+  {
+    state_turn = turn_right;
+    timeDetected = millis();
+    detected = true;
+  }
+  else if(readAnalogSensorData(2)>value_for_black)
+  {
+    state_turn = turn_left;
+    timeDetected = millis();
+    detected = true;
+  }
+  else if(readAnalogSensorData(3)<value_for_white && readAnalogSensorData(2)<value_for_white)
+  {
+    state_turn = go_forward;
+  }
+ 
+}
+void followLine()
+{
+    if(!detected)
+  {
+    detectBlack();
+    motorBackward(5, 100);
+    motorBackward(3, 100);
+    motorForward(2, 100);
+    motorForward(4, 100);
+  }
+if(millis()-timeDetected < time_check && detected)
+{
+  switch(state_turn){
+  case turn_right:
+    motorBackward(5, 25);
+    motorForward(4, 25);
+    motorBackward(3, 100);
+    motorForward(2, 100);
+    break;
+  case turn_left:
+    motorBackward(3, 25);
+    motorForward(2, 25);
+    motorBackward(5, 100);
+    motorForward(4, 100);
+    break;
+  case go_forward:
+    motorBackward(5, 100);
+    motorBackward(3, 100);
+    motorForward(2, 100);
+    motorForward(4, 100);
+    break;
+}
+}
+else
+detected = false;
 }
